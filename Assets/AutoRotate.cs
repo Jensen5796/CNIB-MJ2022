f1d27a64e5b99f78225e2b5d5901cfa374da18f9
Jensen5796 <jensen5796@saskpolytech.ca>
2022-04-28 08:38:17 -0600

    First attempt at auto rotate script, added to prefab

---
+++ Assets/AutoRotate.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoRotate : MonoBehaviour
+{
+
+    Text test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = GetComponent<Text>();
+
+        Transform thisTransform = GetComponent<Transform>();
+        float angleToRotate = FindAngle();
+        thisTransform.Rotate(0, angleToRotate, 0);
+
+        test.text = "Rotated: " + angleToRotate.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private float FindHypotenuse()
+    {
+        float z = transform.position.z;
+        float x = transform.position.x;
+        float hyp = Mathf.Sqrt(Mathf.Pow(z, 2) + Mathf.Pow(x, 2));
+        return hyp;
+    }
+
+    private float FindAngle()
+    {
+        float angle = 0;
+        float z = transform.position.z;
+        float x = transform.position.x;
+
+        //Quadrant 1
+        if (x > 0 && z > 0)
+        {
+            angle = Mathf.Cos(z / FindHypotenuse());
+            
+        }
+        //Quadrant 2
+        if (x < 0 && z > 0)
+        {
+            angle = Mathf.Cos(z / FindHypotenuse());
+            angle *= -1;
+        }
+        //Quadrant 3
+        if (x < 0 && z < 0)
+        {
+            angle = Mathf.Cos(z / FindHypotenuse());
+            angle += 90;
+            angle *= -1;
+        }
+        //Quadrant 4
+        if (x > 0 && z < 0)
+        {
+            angle = Mathf.Cos(z / FindHypotenuse());
+            angle += 90;
+        }
+
+        return angle;
+    }
+}
