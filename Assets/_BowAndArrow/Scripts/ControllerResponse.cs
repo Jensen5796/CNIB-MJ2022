7ac1845876eafdb60e606d82de25e820f54b5d2f
Jensen5796 <Jensen5796@saskpolytech.ca>
2022-05-16 13:17:27 -0600

    Wrote first iteration of Controller Response script
    gets references to L and R controllers,
    returns either N (null), L (left) or R(right).
    Should be called from a loop or update function to act on responses of L or R.

---
+++ Assets/_BowAndArrow/Scripts/ControllerResponse.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ControllerResponse : MonoBehaviour
+{
+
+    ActionBasedController controllerLeft;
+    ActionBasedController controllerRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerLeft = GameObject.Find("LeftHand Contoller").GetComponent<ActionBasedController>();
+        controllerRight = GameObject.Find("RightHand Contoller").GetComponent<ActionBasedController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public char getControllerResponse()
+    {
+        //should be called from an update function so user response can be recorded when it is given
+            //otherwise, set up a loop to continuously call this function if the response returned is 'N'
+
+        char response = 'N'; // null response before assignment
+        if (controllerLeft.selectInteractionState.active)
+        {
+            //Left controller grip button was pressed
+            response = 'L';
+        }
+        else if (controllerRight.selectInteractionState.active)
+        {
+            //Right controller grip button was pressed
+            response = 'R';
+        }
+        return response;
+    }
+}
