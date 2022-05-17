33f77f68f459997d2acc398bce0af4c4223c1556
Jensen5796 <Jensen5796@saskpolytech.ca>
2022-05-17 09:57:10 -0600

    Changed timer ends script and renamed some text objects in scene

--- Assets/_BowAndArrow/Scripts/Extra/TimerEnds.cs
+++ Assets/_BowAndArrow/Scripts/Extra/TimerEnds.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
 
 public class TimerEnds : MonoBehaviour
 {
@@ -9,21 +10,41 @@ public class TimerEnds : MonoBehaviour
     public GameObject MainMenu;
     public GameObject Credits;
     public GameObject timerEnded;
+    ControllerResponse controllerHandler;
+    Text test;
 
-    private XRDirectInteractor rightHandInteractor;
-    private XRDirectInteractor leftHandInteractor;
+    //private XRDirectInteractor rightHandInteractor;
+    //private XRDirectInteractor leftHandInteractor;
     private void Start()
     {
-        GameObject rightHand = GameObject.Find("RightHand Controller");
-        GameObject leftHand = GameObject.Find("LeftHand Controller");
-        rightHandInteractor = rightHand.GetComponent<XRDirectInteractor>();
-        leftHandInteractor = rightHand.GetComponent<XRDirectInteractor>();
+        //GameObject rightHand = GameObject.Find("RightHand Controller");
+        //GameObject leftHand = GameObject.Find("LeftHand Controller");
+        //rightHandInteractor = rightHand.GetComponent<XRDirectInteractor>();
+        //leftHandInteractor = rightHand.GetComponent<XRDirectInteractor>();
 
-        if (rightHandInteractor.isSelectActive) 
+        //if (rightHandInteractor.isSelectActive) 
+        //{
+        //    Main();
+        //}
+        controllerHandler = new ControllerResponse();
+        test = GameObject.Find("Text").GetComponent<Text>();
+        
+    }
+
+    private void Update()
+    {
+        char response = controllerHandler.getControllerResponse();
+        if (response == 'L')
         {
+            //do left side thing
+            test.text = "L side pressed";
+        }
+        else if (response == 'R')
+        {
+            //do right side thing
+            test.text = "R side pressed";
             Main();
         }
-        
     }
     public void Main() 
     {
